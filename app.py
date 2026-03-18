6397ea1bbf48c3bcb63fff58e27675fe997b65ec
Miro Zukina <sschmiro@gmail.com>
2026-03-18 16:17:31 +0000

    new

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# English Route (Home)
+@app.route('/')
+def index_en():
+    return render_template('index.html')
+
+# Croatian Route
+@app.route('/hr')
+def index_hr():
+    return render_template('index_hr.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
